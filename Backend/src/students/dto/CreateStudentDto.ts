c5cb7fdf91de81c424320252efa49eb78e8146b9
Edwin HC <edwinrhc@gmail.com>
2025-11-18 17:45:53 -0500

    type(scope): mensaje corto en presente y claro
    -----------------------------------------
    git config --global core.editor "code --wait"
    (Uso del VSCODE)
    git config --global core.editor "notepad"
    
    Fleet
    git config --global core.editor "'C:/Users/tu usuario/AppData/Local/Programs/Fleet/fleet.exe' --wait"
    
    refactor(dto): Añadir mensajes de error personalizados a CreateStudentDto
    - Se han implementado mensajes personalizados en las validaciones de 'CreateStudentDto' para 'firstName', 'lastName', 'grade' y 'parentId'. Esto mejora la claridad de la respuesta al cliente en caso de errores de validación (BadRequest).

--- Backend/src/students/dto/CreateStudentDto.ts
+++ Backend/src/students/dto/CreateStudentDto.ts
@@ -1,19 +1,19 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 export class CreateStudentDto {
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({message:'EL nombre debe ser un texto'})
+  @IsNotEmpty({message:'El nombre es obligatorio'})
   firstName: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({message:'EL apellido debe ser un texto'})
+  @IsNotEmpty({message:'El apellido es obligatorio'})
   lastName: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({message:'EL grado debe ser un texto'})
+  @IsNotEmpty({message:'El grado es obligatorio'})
   grade: string;
 
-  @IsNumber()
+  @IsNumber({}, { message: 'El ID del padre debe ser un número' })
   parentId: number;
 
 }
