31c277710611e79c769afd154e844f9725384178
Edwin HC <edwinrhc@gmail.com>
2025-11-18 17:05:28 -0500

    chore(): Agregando la dependencia de validators

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Solo permite los campos declarados en el DTO
+      forbidNonWhitelisted: true, // si llega un campo NO permitido, lanza Error
+      transform: true, // Convierte tipo automáticamente (string -> number)
+    }),
+  )
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
