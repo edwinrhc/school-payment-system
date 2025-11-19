512d7596ef51ff5bd698b61086595185e2632709
Edwin HC <edwinrhc@gmail.com>
2025-11-19 18:43:22 -0500

    feat(users): Implementar endpoints GET protegidos con roles de administrador
    Se implementan los endpoints de solo lectura (`findAll`, `find`, `findOne`) en `UsersController`.
    1. Se protege el controlador con `JwtAuthGuard` y `RolesGuard`.
    2. Se restringe el acceso a todos los métodos a usuarios con rol `ADMIN` (`@Roles(UserRole.ADMIN)`).
    3. Se añade lógica en el método `find` para listar todos los usuarios o buscar uno por email usando *query parameters*.
    4. Se añade documentación de Swagger (`@ApiTags`, `@ApiOperation`, `@ApiResponse`).

--- Backend/src/users/users.controller.ts
+++ Backend/src/users/users.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { RolesGuard } from '../auth/guards/RolesGuard';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UsersService } from './users.service';
+import { UserRole } from './entities/user.entity';
+import { Roles } from '../auth/decorators/roles.decorator';
 
+
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard,RolesGuard)
+@ApiTags('Users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  constructor(private readonly usersService: UsersService){}
+
+  /**
+   * GET /users
+   * GET /users?email=email
+   * @param email
+   */
+  @Get()
+  @ApiOperation({summary:'Listar usuarios o buscar por email'})
+  @ApiResponse({status: 200, description: 'Retorna  los usuarios o  un usuario por email'})
+  @ApiResponse({status: 404, description: 'Usuarios  no encontrados o  no se encontró usuario por email'})
+  @Roles(UserRole.ADMIN)
+  find(@Query('email') email?: string){
+    if(email){
+      return this.usersService.findByEmail(email);
+    }
+    return this.usersService.findAll();
+  }
+
+  // @Get('email/:email')
+  // @ApiOperation({summary:'Obtener usuario por email'})
+  // @ApiResponse({status: 200, description: 'Obtener usuario por email'})
+  // @ApiResponse({status: 404, description: 'Usuario no encontrado'})
+  // @Roles(UserRole.ADMIN)
+  // findByEmail(@Param('email') email:string){
+  //   return this.usersService.findByEmail(email);
+  // }
+
+
+  /**
+   * GET /users/:id
+   */
+  @Get(':id')
+  @ApiOperation({summary:'Obtener usuario por ID'})
+  @ApiResponse({status: 200, description: 'Usuario encontrado'})
+  @ApiResponse({status: 404, description: 'Usuario no encontrado'})
+  @Roles(UserRole.ADMIN)
+  findOne(@Param('id') id: string){
+    return this.usersService.findOne(+id);
+  }
+
+
+
+
+}
