4850e66ec93a6add2689c670e48e8f318c1b056f
Edwin HC <edwinrhc@gmail.com>
2025-11-19 11:24:38 -0500

    feat(login): Implementar el login.dto

---
+++ Backend/src/auth/dto/login.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+
+export class LoginDto {
+
+
+  @ApiProperty({
+    example:'correo@correo.com'
+  })
+  @IsEmail()
+  email: string;
+
+
+  @ApiProperty({
+    example:'123456'
+  })
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+}
