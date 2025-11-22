4ffa91d06cfdebaa7c90991e0d96c6c141ace5f1
Edwin HC <edwinrhc@gmail.com>
2025-11-21 19:04:36 -0500

    feat: Agregar validaciones al DTO ForgotPasswordDto - Se agrega @IsEmail para validar formato de correo - Se agrega @IsNotEmpty para asegurar que el campo email no esté vacío - Se incluyen mensajes personalizados en español para mejrar la retroalimentación al usuario.

--- Backend/src/users/dto/ForgotPasswordDto.ts
+++ Backend/src/users/dto/ForgotPasswordDto.ts
@@ -1,5 +1,8 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 
 export class ForgotPasswordDto {
+  @IsEmail({}, { message: 'Debe ser un email válido' })
+  @IsNotEmpty({ message: 'El email es obligatorio' })
   email: string
 }
