1b08b97fc2e71621e84107726ffcd3447f7fe58d
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-12-28 14:49:21 -0500

    feat(frontend): add base layout with navbar and home page

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
+import {HomeComponent} from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+   path:'',
+   component: MainLayoutComponent,
+   children:[
+     {path:'', component: HomeComponent}
+   ]
+
+  }
+];
