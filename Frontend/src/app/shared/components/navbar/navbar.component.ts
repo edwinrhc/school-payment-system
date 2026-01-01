123fc4105b49118444ddbe447dda5b2c8aee80ea
Edwin Huamanttupa <edwinrhc@gmail.com>
2026-01-01 17:03:23 -0500

    feat(frontend): add logout option to navbar with auth state handling

--- Frontend/src/app/shared/components/navbar/navbar.component.ts
+++ Frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
+import {NgIf} from '@angular/common';
+import {AuthService} from '../../../features/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+              ) {}
+
+  logout(){
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
