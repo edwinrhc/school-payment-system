b46d06fc08c2d00280183d37dee354f4d2b90b15
Edwin Huamanttupa <edwinrhc@gmail.com>
2026-01-01 17:20:50 -0500

    fix(frontend): correct home route navigation to root path

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,19 +1,26 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
 import {HomeComponent} from './features/home/home.component';
 import {LoginComponent} from './features/auth/pages/login/login.component';
 import {RegisterComponent} from './features/auth/pages/register/register.component';
+import {authGuard} from './core/guards/auth.guard';
 
 export const routes: Routes = [
 
   {
-   path:'',
-   component: MainLayoutComponent,
-   children:[
-     {path:'', component: HomeComponent},
-     {path:'login', component: LoginComponent},
-     {path:'register', component: RegisterComponent},
-   ]
+    path: '',
+    component: MainLayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      { path: '', component: HomeComponent },
+    ],
+  },
 
-  }
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+
+  { path: '**', redirectTo: '' }
+
+
+
 ];
