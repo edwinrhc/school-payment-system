3b72f54e9bf787b5847dad5e48f20061c5ac73f4
Edwin Huamanttupa <edwinrhc@gmail.com>
2026-01-01 23:07:07 -0500

    style: design a new style  home

--- Frontend/src/app/features/home/home.component.ts
+++ Frontend/src/app/features/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../auth/services/auth.service';
+import {UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    UpperCasePipe
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  user: any;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.getUser();
+  }
 
 }
